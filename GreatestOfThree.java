7dbba6f51d18f879565962722c6448659b079067
Shreyansh36m <2303031560039@paruluniversity.ac.in>
2026-03-17 18:30:36 +0530

    Add files via upload
    This program uses conditional statements and logical operators to find out the greatest number out the three numbers given input.

---
+++ GreatestOfThree.java
@@ -0,0 +1,21 @@
+package Java_DS_Algo;
+
+import java.util.Scanner;
+
+public class GreatestOfThree {
+    // Take input of three numbers and print the greatest
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the first number: ");
+        int num3 = sc.nextInt();
+
+        if(num1 > num2 && num2 > num3) System.out.println(num1+" is the greatest number.");
+        else if (num2 < num3) System.out.println(num3+" is the greatest number.");
+        else if (num2 > num3) System.out.println(num2+" is the greatest number.");
+    }
+}

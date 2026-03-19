93539acb0654b7ea73acafef47d70c2bab42598d
Shreyansh36m <2303031560039@paruluniversity.ac.in>
2026-03-19 23:55:27 +0530

    Output an Array
    This program demonstrates simple use of loop to output an array.

---
+++ Output.java
@@ -0,0 +1,13 @@
+package Arrayz;
+
+public class Output {
+    // Output Array
+
+    public void printing(int[] arr){
+        System.out.println("The elements are: ");
+        for (int i = 0; i < arr.length; i++) {
+            
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
